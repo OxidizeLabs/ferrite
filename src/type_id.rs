5887363fd3d142a27e9e31727df20d6173b864a9
TK <thomas.korrison@clausematch.com>
2024-07-16 14:41:02 +0100

    Add TypeId enum

---
+++ src/type_id.rs
@@ -0,0 +1,20 @@
+// Every possible SQL type ID
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TypeId {
+    // Invalid = 0,
+    Boolean,
+    // Integer,
+    // Decimal,
+    // Varchar,
+    // Timestamp,
+    // Vector,
+    // TinyInt,
+    // SmallInt,
+    // BigInt
+}
+
+
+// fn main() {
+//     let type_id = TypeId::Integer;
+//     println!("{:?}", type_id);
+// }
