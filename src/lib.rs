7a354932a4b9f45c724349cbe80dcf219aa6c025
TK <thomas.korrison@clausematch.com>
2024-07-16 14:41:32 +0100

    Update lib, main and Cargo to compile successfully

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod types;
+pub mod value;
+pub mod type_id;
+pub mod boolean_type;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+extern crate tkdb;
+
+use tkdb::types::get_instance;
+use tkdb::value::Value;
+use tkdb::type_id::TypeId;
 
 fn main() {
-    println!("Hello, World!")
+
+    let _boolean_type = get_instance(TypeId::Boolean);
+    let val = Value::new(TypeId::Boolean);
+    println!("{:?}", val);
 }
