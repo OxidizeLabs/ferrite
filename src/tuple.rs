29eb3f0526d092cc0e58c5f18d7f280b72ca5640
TK <thomas.korrison@clausematch.com>
2024-07-17 18:34:34 +0100

    Implement tuple constructor

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,29 +1,62 @@
 use std::mem::size_of;
-use std::ptr::null;
 use limits::DB_VALUE_NULL;
 use value::Value;
 use schema::Schema;
-use type_id::TypeId;
 
 pub struct Tuple {
     values: Vec<Value>,
     schema: Schema,
     rid: u32,
-    data: Vec<char>
+    data: Vec<u8>,
 }
 
 impl Tuple {
-    pub fn new(values: Vec<Value>, schema: &Schema) {
-        assert_eq!(values.len(), schema.get_column_count(), "Values length does not match schema column count");
+    pub fn new(values: Vec<Value>, schema: Schema, rid: u32) -> Self {
+        assert_eq!(
+            values.len(),
+            schema.get_column_count(),
+            "Values length does not match schema column count"
+        );
 
+        // 1. Calculate the size of the tuple.
         let mut tuple_size = schema.get_inlined_storage_size() as usize;
-        for value in &values {
-            let mut len = value.get_storage_size();
+        for &i in schema.get_unlined_columns() {
+            let mut len = values[i as usize].get_storage_size();
             if len == DB_VALUE_NULL {
                 len = 0;
             }
             tuple_size += size_of::<u32>() + len as usize;
         }
+
+        // 2. Allocate memory.
+        let mut data = vec![0; tuple_size];
+
+        // 3. Serialize each attribute based on the input value.
+        let mut offset = schema.get_inlined_storage_size() as usize;
+
+        for (i, value) in values.iter().enumerate() {
+            if let Some(col) = schema.get_column(i) {
+                if !col.is_inlined() {
+                    // Serialize relative offset, where the actual varchar data is stored.
+                    let offset_ptr = &mut data[col.get_offset() as usize..col.get_offset() as usize + size_of::<u32>()];
+                    offset_ptr.copy_from_slice(&(offset as u32).to_ne_bytes());
+
+                    // Serialize varchar value, in place (size + data).
+                    Value::serialize_to(&mut data[offset..]);
+                    let mut len = value.get_storage_size();
+                    if len == DB_VALUE_NULL {
+                        len = 0;
+                    }
+                    offset += size_of::<u32>() + len as usize;
+                } else {
+                    Value::serialize_to(&mut data[col.get_offset() as usize..]);
+                }
+            } else {
+                panic!("Column at index {} not found", i);
+            }
+        }
+
+        Tuple { values, schema, rid, data }
     }
 
 
