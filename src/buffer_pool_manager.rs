ad016d6964f69b862001fc5dccd5269518fcfad9
TK <thomas.korrison@clausematch.com>
2024-07-24 00:36:06 +0100

    Update buffer_pool_manager.rs
    Add implementation for methods:
    - new_page
    - delete page
    - allocate_page

--- src/buffer_pool_manager.rs
+++ src/buffer_pool_manager.rs
@@ -1,14 +1,12 @@
-use std::collections::{HashMap, VecDeque};
-use std::future::poll_fn;
-use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex, RwLock};
 use std::sync::atomic::{AtomicI32, Ordering};
-use config::LRUK_REPLACER_K;
+use config::{FrameId, PageId};
 use disk_manager::DiskManager;
-use lru_k_replacer::LRUKReplacer;
+use lru_k_replacer::{AccessType, LRUKReplacer};
+use lru_k_replacer::AccessType::Lookup;
 use page::Page;
 
-type PageId = i32;
-type FrameId = usize;
 const INVALID_PAGE_ID: PageId = -1;
 
 struct LogManager {}
@@ -17,49 +15,27 @@ struct BasicPageGuard {}
 struct ReadPageGuard {}
 struct WritePageGuard {}
 
-#[derive(Clone, Copy)]
-enum AccessType {
-    Unknown,
-}
-
 pub struct BufferPoolManager {
     pool_size: usize,
     next_page_id: AtomicI32,
-    pages: Vec<Page>,
+    pages: Arc<RwLock<Vec<Option<Page>>>>, // Use RwLock for concurrent reads/writes
     page_table: Arc<Mutex<HashMap<PageId, FrameId>>>,
-    replacer: Arc<Mutex<LRUKReplacer>>,
-    free_list: Arc<Mutex<VecDeque<FrameId>>>,
-    latch: Arc<Mutex<()>>,
+    replacer: Arc<Mutex<LRUKReplacer>>, // Use Mutex for exclusive access
+    free_list: Arc<Mutex<Vec<FrameId>>>,
     disk_manager: Arc<DiskManager>,
-    log_manager: Option<Arc<LogManager>>,
     write_back_cache: Option<Arc<WriteBackCache>>,
 }
 
-/**
- * BufferPoolManager reads disk pages to and from its internal buffer pool.
- */
 impl BufferPoolManager {
-    pub fn new(pool_size: usize, disk_manager: Arc<DiskManager>, replacer_k: usize, log_manager: Option<Arc<LogManager>>) -> Self {
-        /**
-         * @brief Creates a new BufferPoolManager.
-         * @param pool_size the size of the buffer pool
-         * @param disk_manager the disk manager
-         * @param replacer_k the LookBack constant k for the LRU-K replacer
-         * @param log_manager the log manager (for testing only: nullptr = disable logging). Please ignore this for P1.
-         */
-
-        let page_ = Page::new(0);
-        let replacer_ = LRUKReplacer::new(0, replacer_k);
+    pub fn new(pool_size: usize, disk_manager: Arc<DiskManager>, replacer: LRUKReplacer) -> Self {
         Self {
             pool_size,
             next_page_id: AtomicI32::new(0),
-            pages: Vec::with_capacity(pool_size),
+            pages: Arc::new(RwLock::new(vec![None; pool_size])),
             page_table: Arc::new(Mutex::new(HashMap::new())),
-            replacer: Arc::new(Mutex::new(replacer_)),
-            free_list: Arc::new(Mutex::new(VecDeque::new())),
-            latch: Arc::new(Mutex::new(())),
+            replacer: Arc::new(Mutex::new(replacer)),
+            free_list: Arc::new(Mutex::new(Vec::with_capacity(pool_size))),
             disk_manager,
-            log_manager,
             write_back_cache: None,
         }
     }
@@ -68,96 +44,138 @@ impl BufferPoolManager {
         self.pool_size
     }
 
-    pub fn get_pages(&self) -> &Vec<Page> {
-        &self.pages
+    pub fn get_pages(&self) -> Arc<RwLock<Vec<Option<Page>>>> {
+        self.pages.clone()
     }
 
-    pub fn new_page(&mut self, page_id: PageId) -> &Page {
-        /**
-         * TODO(P1): Add implementation
-         *
-         * @brief Create a new page in the buffer pool. Set page_id to the new page's id, or nullptr if all frames
-         * are currently in use and not evictable (in another word, pinned).
-         *
-         * You should pick the replacement frame from either the free list or the replacer (always find from the free list
-         * first), and then call the AllocatePage() method to get a new page id. If the replacement frame has a dirty page,
-         * you should write it back to the disk first. You also need to reset the memory and metadata for the new page.
-         *
-         * Remember to "Pin" the frame by calling replacer.SetEvictable(frame_id, false)
-         * so that the replacer wouldn't evict the frame before the buffer pool manager "Unpin"s it.
-         * Also, remember to record the access history of the frame in the replacer for the lru-k algorithm to work.
-         *
-         * @param[out] page_id id of created page
-         * @return nullptr if no new pages could be created, otherwise pointer to new page
-         */
+    pub fn new_page(&self) -> Option<Page> {
+        // Attempt to find a free frame first
+        let frame_id = {
+            let mut free_list = self.free_list.lock().unwrap();
+            if let Some(frame_id) = free_list.pop() {
+                frame_id
+            } else {
+                // Lock the replacer to evict a frame
+                let mut replacer = self.replacer.lock().unwrap();
+                replacer.evict()?
+            }
+        };
 
-        // get replacement frame from free list then/or replacer
+        // Remove the old page from the page table if necessary
+        let old_page_id = {
+            let page_table = self.page_table.lock().unwrap();
+            page_table.iter().find_map(|(&page_id, &fid)| {
+                if fid == frame_id {
+                    Some(page_id)
+                } else {
+                    None
+                }
+            })
+        };
 
+        if let Some(old_page_id) = old_page_id {
+            if let Some(page) = self.pages.read().unwrap().iter().find(|p| {
+                p.as_ref().map_or(false, |pg| pg.get_page_id() == old_page_id)
+            }) {
+                if page.as_ref().unwrap().is_dirty() {
+                    self.disk_manager.write_page(old_page_id, page.as_ref().unwrap().get_data());
+                }
+                let mut page_table = self.page_table.lock().unwrap();
+                page_table.remove(&old_page_id);
+            }
+        }
 
-        // get page_id from calling allocate_page()
-        unimplemented!()
+        // Allocate a new page ID
+        let new_page_id = self.allocate_page();
 
+        // Create a new page and reset its metadata
+        let mut new_page = Page::new(new_page_id);
+        new_page.set_dirty(false);
+        new_page.reset_memory();
+
+        // Insert the new page into the pages map and page table
+        let mut pages = self.pages.write().unwrap();
+        if frame_id as usize >= pages.len() {
+            pages.resize_with(frame_id as usize + 1, || None);
+        }
+        pages[frame_id as usize] = Some(new_page.clone());
+
+        let mut page_table = self.page_table.lock().unwrap();
+        page_table.insert(new_page_id, frame_id);
+
+        // Lock the replacer to mark this frame as non-evictable
+        let mut replacer = self.replacer.lock().unwrap();
+        replacer.set_evictable(frame_id, false);
+        replacer.record_access(frame_id, Lookup);
+
+        Some(new_page)
     }
 
     pub fn new_page_guarded(&self, page_id: PageId) -> Option<BasicPageGuard> {
-        // Implementation here
-        None
+        unimplemented!()
     }
 
     pub fn fetch_page(&self, page_id: PageId, access_type: AccessType) -> Option<&Page> {
-        // Implementation here
-        None
+        unimplemented!()
     }
 
     pub fn fetch_page_basic(&self, page_id: PageId) -> Option<BasicPageGuard> {
-        // Implementation here
-        None
+        unimplemented!()
     }
 
     pub fn fetch_page_read(&self, page_id: PageId) -> Option<ReadPageGuard> {
-        // Implementation here
-        None
+        unimplemented!()
     }
 
     pub fn fetch_page_write(&self, page_id: PageId) -> Option<WritePageGuard> {
-        // Implementation here
-        None
+        unimplemented!()
     }
 
     pub fn unpin_page(&self, page_id: PageId, is_dirty: bool, access_type: AccessType) -> bool {
-        // Implementation here
-        false
+        unimplemented!()
     }
 
     pub fn flush_page(&self, page_id: PageId) -> bool {
-        // Implementation here
-        false
+        unimplemented!()
     }
 
     pub fn flush_all_pages(&self) {
-        // Implementation here
+        unimplemented!()
     }
 
     pub fn delete_page(&self, page_id: PageId) -> bool {
-        // Implementation here
+        if let Some(frame_id) = self.page_table.lock().unwrap().get(&page_id) {
+            let mut pages = self.pages.write().unwrap();
+            if let Some(page) = pages.get_mut(*frame_id as usize).and_then(Option::as_mut) {
+                page.reset_memory();
+                let mut replacer = self.replacer.lock().unwrap();
+                replacer.remove(*frame_id);
+                self.free_list.lock().unwrap().push(*frame_id);
+                return true;
+            }
+        }
         false
     }
 
-    fn allocate_page(&self) -> PageId {
-        // Implementation here
-        INVALID_PAGE_ID
+    pub fn allocate_page(&self) -> PageId {
+        self.next_page_id.fetch_add(1, Ordering::SeqCst)
     }
 
-    fn deallocate_page(&self, page_id: PageId) {
-        // Implementation here
+    pub fn deallocate_page(&mut self, page_id: PageId) {
+        unimplemented!()
     }
 }
 
 // fn main() {
 //     // Example usage of BufferPoolManager
-//     let disk_manager = Arc::new(DiskManager {});
-//     let log_manager = Arc::new(LogManager {});
-//     let buffer_pool_manager = BufferPoolManager::new(100, disk_manager, 2, Some(log_manager));
+//     let disk_manager = Arc::new(disk_manager::DiskManager {});
+//     let replacer = Arc::new(lru_k_replacer::LRUKReplacer {});
+//     let mut buffer_pool_manager = BufferPoolManager::new(100, disk_manager, replacer);
 //
-//     // Further code...
+//     // Create a new page
+//     if let Some(new_page) = buffer_pool_manager.new_page() {
+//         println!("Created new page: {:?}", new_page.lock().unwrap().get_page_id());
+//     }
+//
+//     // Further usage...
 // }
