2f725709a4442baa254bbf16060ce588e2fe2b32
TK <thomas.korrison@clausematch.com>
2024-07-24 00:34:39 +0100

    Update lru_k_replacer.rs
    Add implementation for methods:
    - set_evictable
    - record_access
    - evict
    - current_time

--- src/lru_k_replacer.rs
+++ src/lru_k_replacer.rs
@@ -1,7 +1,9 @@
 use config::FrameId;
 use std::collections::HashMap;
 use std::sync::Mutex;
+use std::time::{SystemTime, UNIX_EPOCH};
 
+#[derive(Clone, Copy)]
 pub enum AccessType {
     Unknown = 0,
     Lookup,
@@ -10,21 +12,21 @@ pub enum AccessType {
 }
 
 #[derive(Hash, Eq, PartialEq, Debug)]
-pub struct LRUKNode {
+pub struct Frame {
     /** History of last seen K timestamps of this page. Least recent timestamp stored in front. */
-    history_: Vec<usize>,
-    k_: usize,
-    fid_: FrameId,
-    is_evictable_: bool
+    access_times: Vec<u64>,
+    k: usize,
+    fid: FrameId,
+    is_evictable: bool
 }
 
 pub struct LRUKReplacer {
-    node_store_: HashMap<FrameId, LRUKNode>,
-    current_timestamp_: usize,
+    frame_store: HashMap<FrameId, Frame>,
+    current_timestamp: usize,
     curr_size_: usize,
-    replacer_size_: usize,
-    k_: usize,
-    latch_: Mutex<usize>
+    replacer_size: usize,
+    k: usize,
+    latch: Mutex<usize>
 }
 
 /**
@@ -40,26 +42,24 @@ pub struct LRUKReplacer {
  */
 impl LRUKReplacer {
     pub fn new(num_frames: usize, k: usize) -> Self {
-        let node_store = LRUKNode {
-            history_: vec![],
-            k_: 0,
-            fid_: 0,
-            is_evictable_: false,
+        let frame = Frame {
+            access_times: vec![],
+            k: 0,
+            fid: 0,
+            is_evictable: false,
         };
 
         Self {
-            node_store_: HashMap::from([
-                (0, node_store)
-            ]),
-            current_timestamp_: 0,
+            frame_store: HashMap::from([(0, frame)]),
+            current_timestamp: 0,
             curr_size_: 0,
-            replacer_size_: num_frames,
-            k_: k,
-            latch_: Mutex::new(0),
+            replacer_size: num_frames,
+            k,
+            latch: Mutex::new(0),
         }
     }
 
-    pub fn evict(frame_id: &FrameId) -> bool {
+    pub fn evict(&mut self) -> Option<FrameId> {
         /**
          * TODO(P1): Add implementation
          *
@@ -76,11 +76,48 @@ impl LRUKReplacer {
          * @param[out] frame_id id of frame that is evicted.
          * @return true if a frame is evicted successfully, false if no frames can be evicted.
          */
-        unimplemented!()
+        let mut max_k_distance = 0;
+        let mut victim_frame_id = None;
+
+        for (id, frame) in &self.frame_store {
+            if !frame.is_evictable {
+                continue;
+            }
+
+            let k_distance = if frame.access_times.len() < self.k {
+                u64::MAX
+            } else {
+                // Calculate backward k-distance
+                let len = frame.access_times.len();
+                frame.access_times[len - self.k] // k-th last access time
+            };
+
+            // Determine the frame to evict
+            if k_distance > max_k_distance {
+                max_k_distance = k_distance;
+                victim_frame_id = Some(*id);
+            } else if k_distance == max_k_distance {
+                // If k_distance is the same, choose the frame with the earliest access time
+                if let Some(current_victim_id) = victim_frame_id {
+                    let current_victim_frame = &self.frame_store[&current_victim_id];
+                    if frame.access_times.first() < current_victim_frame.access_times.first() {
+                        victim_frame_id = Some(*id);
+                    }
+                }
+            }
+        }
+
+        // Evict the selected frame
+        if let Some(victim_id) = victim_frame_id {
+            self.frame_store.remove(&victim_id);
+            Some(victim_id)
+        } else {
+            None
+        }
 
     }
 
-    pub fn record_access(frame_id: FrameId, access_type: AccessType) {
+    pub fn record_access(&mut self, frame_id: FrameId, access_type: AccessType) {
         /**
          * TODO(P1): Add implementation
          *
@@ -94,10 +131,19 @@ impl LRUKReplacer {
          * @param access_type type of access that was received. This parameter is only needed for
          * leaderboard tests.
          */
-        unimplemented!()
+        let now = Self::current_time();
+        self.frame_store
+            .entry(frame_id)
+            .and_modify(|frame| frame.access_times.push(now))
+            .or_insert(Frame {
+                is_evictable: true,
+                access_times: vec![now],
+                k: 0,
+                fid: 0,
+            });
     }
 
-    pub fn set_evictable(frame_id: FrameId, set_evictable: bool) {
+    pub fn set_evictable(&mut self, frame_id: FrameId, set_evictable: bool) {
         /**
          * TODO(P1): Add implementation
          *
@@ -115,10 +161,12 @@ impl LRUKReplacer {
          * @param frame_id id of frame whose 'evictable' status will be modified
          * @param set_evictable whether the given frame is evictable or not
          */
-        unimplemented!()
+        if let Some(frame) = self.frame_store.get_mut(&frame_id) {
+            frame.is_evictable = set_evictable;
+        }
     }
 
-    pub fn remove(frame_id: FrameId) {
+    pub fn remove(&self, frame_id: FrameId) {
         /**
          * TODO(P1): Add implementation
          *
@@ -139,7 +187,7 @@ impl LRUKReplacer {
         unimplemented!()
     }
 
-    pub fn size() -> usize {
+    pub fn size(self) -> usize {
         /**
          * TODO(P1): Add implementation
          *
@@ -148,6 +196,14 @@ impl LRUKReplacer {
          * @return size_t
          */
         unimplemented!()
+    }
+
+    // Helper function to get current time in milliseconds since epoch
+    fn current_time() -> u64 {
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u64
     }
 }
 
