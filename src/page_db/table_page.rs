7146ea698e792f20e9948e83389828c8afe85ef4
TK <thomas.korrison@clausematch.com>
2024-07-24 23:11:31 +0100

    Update Hashmap with xxhash hashing algorithm

--- src/page_db/table_page.rs
+++ src/page_db/table_page.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
-use std::option::Option;
-
+use crate::common::config::PageId;
 use crate::table::tuple::Tuple;
-
-type PageId = i32;
+use std::collections::HashMap;
+use std::hash::{BuildHasherDefault, Hasher};
+use std::option::Option;
+use xxhash_rust::xxh3::Xxh3;
 
 #[derive(Clone, Debug)]
 struct TupleMeta {}
@@ -11,27 +11,27 @@ struct TupleMeta {}
 #[derive(Clone, Debug, Hash, Eq, PartialEq)]
 struct RID {}
 
+// Create a type alias for our custom hasher
+type XxHashBuilder = BuildHasherDefault<Xxh3Hasher>;
+
 pub struct TablePage {
     page_start: Vec<u8>,
     next_page_id: PageId,
     num_tuples: u16,
     num_deleted_tuples: u16,
-    tuple_info: HashMap<RID, (u16, u16, TupleMeta)>,
+    tuple_info: HashMap<RID, (TupleMeta, Tuple), XxHashBuilder>,
 }
 
 impl TablePage {
     pub fn new(page_id: PageId) -> Self {
+        let map = HashMap::with_hasher(BuildHasherDefault::<Xxh3Hasher>::default());
         Self {
             page_start: vec![],
             next_page_id: page_id,
             num_tuples: 0,
             num_deleted_tuples: 0,
-            tuple_info: HashMap::new(),
+            tuple_info: map,
         }
-    }
-
-    pub fn init(&mut self) {
-        unimplemented!()
     }
 
     pub fn get_num_tuples(&self) -> u32 {
@@ -58,12 +58,18 @@ impl TablePage {
         unimplemented!()
     }
 
-    pub fn get_tuple(&self, rid: &RID) -> Option<(TupleMeta, Tuple)> {
-        unimplemented!()
+    pub fn get_tuple(&self, rid: &RID) -> Option<(&TupleMeta, &Tuple)> {
+        match self.tuple_info.get(rid).unwrap() {
+            (TupleMeta, Tuple) => Some((TupleMeta, Tuple)),
+            _ => None,
+        }
     }
 
-    pub fn get_tuple_meta(&self, rid: &RID) -> Option<TupleMeta> {
-        unimplemented!()
+    pub fn get_tuple_meta(&self, rid: &RID) -> Option<&TupleMeta> {
+        match self.tuple_info.get(rid).unwrap() {
+            (TupleMeta, _Tuple) => Some(TupleMeta),
+            _ => None,
+        }
     }
 
     pub fn update_tuple_in_place_unsafe(&mut self, meta: &TupleMeta, tuple: &Tuple, rid: &RID) {
@@ -71,9 +77,25 @@ impl TablePage {
     }
 }
 
+// Custom hasher struct to wrap Xxh3 hasher
+#[derive(Default)]
+struct Xxh3Hasher {
+    hasher: Xxh3,
+}
+
+impl Hasher for Xxh3Hasher {
+    fn finish(&self) -> u64 {
+        self.hasher.clone().digest()
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        self.hasher.update(bytes);
+    }
+}
+
 // fn main() {
 //     // Example usage of TablePage
-//     let mut table_page = TablePage::new();
+//     let mut table_page = TablePage::new(PageId::new());
 //     table_page.init();
 //
 //     // Further code...
