8ab89dae1b88aeaf09e9c806b2dfffe786348381
TK <thomas.korrison@clausematch.com>
2024-07-27 11:58:19 +0100

    Update buffer_pool_manager.rs and tests
    - Use DiskScheduler over DiskManager to read/write pages
    - Implementation add for fetch_page method

--- src/tests/buffer_pool_manager_tests.rs
+++ src/tests/buffer_pool_manager_tests.rs
@@ -4,15 +4,27 @@ use crate::common::config::DB_PAGE_SIZE;
 use crate::disk::disk_manager::DiskManager;
 use rand::Rng;
 use std::sync::{Arc, Mutex};
+use crate::disk::disk_scheduler::DiskScheduler;
+use std::fmt::Write;
+
+// Print the data for debugging
+fn format_slice(slice: &[u8]) -> String {
+    let mut s = String::new();
+    for byte in slice {
+        write!(&mut s, "{:02x} ", byte).unwrap();
+    }
+    s
+}
 
 #[test]
 fn binary_data_test() {
-    let db_name = "test.db";
-    let db_log = "test.log";
+    let db_name = "test2.db";
+    let db_log = "test2.log";
     let buffer_pool_size = 10;
     let disk_manager = Arc::new(DiskManager::new(db_name, db_log));
+    let disk_scheduler = Arc::new(DiskScheduler::new(disk_manager));
     let replacer = Arc::new(Mutex::new(LRUKReplacer::new(7, 2)));
-    let bpm = BufferPoolManager::new(buffer_pool_size, disk_manager.clone(), replacer);
+    let bpm = BufferPoolManager::new(buffer_pool_size, disk_scheduler.clone(), replacer);
 
     let page_id_temp = 0;
     let page0 = bpm.new_page();
@@ -21,19 +33,22 @@ fn binary_data_test() {
     assert!(page0.is_some());
     assert_eq!(0, page_id_temp);
 
+    // Generate and fill random binary data
     let mut rng = rand::thread_rng();
     let mut random_binary_data = [0u8; DB_PAGE_SIZE];
     rng.fill(&mut random_binary_data[..]);
 
-    // Insert terminal characters both in the middle and at end
+    // Insert terminal characters both in the middle and at the end
     random_binary_data[DB_PAGE_SIZE / 2] = 0;
     random_binary_data[DB_PAGE_SIZE - 1] = 0;
 
     // Scenario: Once we have a page, we should be able to read and write content.
     {
-        let mut page0 = &page0.unwrap();
-        page0.get_data().copy_from_slice(&random_binary_data);
-        assert_eq!(random_binary_data, page0.get_data());
+        let page0 = page0.unwrap();
+        let mut page_data = page0.get_data();
+        page_data.copy_from_slice(&random_binary_data);
+
+        assert_eq!(random_binary_data, page_data);
     }
 
     // Scenario: We should be able to create new pages until we fill up the buffer pool.
@@ -46,7 +61,7 @@ fn binary_data_test() {
         assert!(bpm.new_page().is_none());
     }
 
-    // Scenario: After unpinning pages {0, 1, 2, 3, 4}, we should be able to create 5 new pages
+    // Scenario: After unpinning pages {0, 1, 2, 3, 4}, we should be able to create 5 new pages.
     for i in 0..5 {
         assert!(bpm.unpin_page(i, true, AccessType::Lookup));
         bpm.flush_page(i);
@@ -57,15 +72,20 @@ fn binary_data_test() {
     }
 
     // Scenario: We should be able to fetch the data we wrote a while ago.
-    let page0 = bpm.fetch_page(0, AccessType::Lookup);
-    // assert!(page0.clone().type_id());
+    let page0 = bpm.fetch_page(0);
     let binding = page0.clone();
-    let page0 = page0.read().unwrap();
-    assert_eq!(random_binary_data, page0.get_data());
+    let binding = binding.unwrap();
+    let page0 = binding.read().unwrap();
+    let fetched_data = page0.get_data();
+
+    // Print the fetched data for debugging
+    println!("Fetched Data:    {}", format_slice(&fetched_data));
+
+    assert_eq!(random_binary_data, fetched_data);
 
     bpm.unpin_page(0, true, AccessType::Lookup);
 
     // Shutdown the disk manager and remove the temporary file we created.
-    &disk_manager.shut_down();
-    std::fs::remove_file("test.db").unwrap();
+    disk_scheduler.clone().shut_down();
+    std::fs::remove_file("test2.db").unwrap();
 }
