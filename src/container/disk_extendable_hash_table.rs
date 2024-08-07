b23b255d768e9699f1d1dd68a48d4904c0e22ba9
TK <thomas.korrison@clausematch.com>
2024-08-07 10:34:16 +0100

    Add disk hash table templates

--- src/container/disk_extendable_hash_table.rs
+++ src/container/disk_extendable_hash_table.rs
@@ -1,109 +1,193 @@
+use std::cmp::Ordering;
+use std::fmt::Debug;
+use std::vec::Vec;
+use std::string::String;
 use std::hash::Hash;
+use std::marker::PhantomData;
+use crate::buffer::buffer_pool_manager::BufferPoolManager;
+use crate::concurrency::transaction::Transaction;
+use crate::container::hash_function::HashFunction;
+use crate::storage::index::generic_key::Comparator;
+use crate::storage::page::{
+    extendable_hash_table_bucket_page::ExtendableHTableBucketPage,
+    extendable_hash_table_header_page::ExtendableHTableHeaderPage,
+    extendable_hash_table_directory_page::ExtendableHTableDirectoryPage
+};
+use crate::storage::page::page_guard::ReadPageGuard;
 
-const HTABLE_HEADER_MAX_DEPTH: u32 = 32;
-const HTABLE_DIRECTORY_MAX_DEPTH: u32 = 32;
-// const HTableBucketArraySize: usize = 128;
+pub struct DiskExtendableHashTable<K, V, C> {
+    index_name: String,
+    bpm: BufferPoolManager,
+    cmp: C,
+    hash_fn: HashFunction<K>,
+    header_max_depth: u32,
+    directory_max_depth: u32,
+    bucket_max_size: usize,
+    header_page_id: u32,
+    _marker: PhantomData<V>
+}
 
-pub struct DiskExtendableHashTable;
+impl<K, V, C> DiskExtendableHashTable<K, V, C>
+where
+    K: Eq + Hash + Clone + Debug,
+    V: Clone + Debug,
+    C: Fn(&K, &K) -> Ordering + Comparator<K> + Clone,
+{
+    /// Creates a new DiskExtendibleHashTable.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - The name of the hash table.
+    /// * `bpm` - Buffer pool manager to be used.
+    /// * `cmp` - Comparator for keys.
+    /// * `hash_fn` - The hash function.
+    /// * `header_max_depth` - The max depth allowed for the header page.
+    /// * `directory_max_depth` - The max depth allowed for the directory page.
+    /// * `bucket_max_size` - The max size allowed for the bucket page array.
+    pub fn new(
+        name: String,
+        bpm: BufferPoolManager,
+        cmp: C,
+        hash_fn: HashFunction<K>,
+        header_max_depth: u32,
+        directory_max_depth: u32,
+        bucket_max_size: usize,
+    ) -> Self {
+        Self {
+            index_name: name,
+            bpm,
+            cmp,
+            hash_fn,
+            header_max_depth,
+            directory_max_depth,
+            bucket_max_size,
+            header_page_id: 0, // This should be set appropriately
+            _marker: Default::default(),
+        }
+    }
 
+    /// Inserts a key-value pair into the hash table.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to insert.
+    /// * `value` - The value to be associated with the key.
+    /// * `transaction` - The current transaction (optional).
+    ///
+    /// # Returns
+    ///
+    /// `true` if the insert succeeded, `false` otherwise.
+    pub fn insert(&self, key: &K, value: &V, transaction: Option<&Transaction>) -> bool {
+        // TODO(P2): Add implementation
+        unimplemented!()
+    }
 
-// impl<K, V, C> DiskExtendableHashTable<K, V, C>
-// where
-//     K: KeyType,
-//     V: ValueType,
-//     C: KeyComparator<K>,
-// {
-//     pub fn new(
-//         name: String,
-//         bpm: Arc<BufferPoolManager>,
-//         cmp: C,
-//         hash_fn: HashFunction<K>,
-//         header_max_depth: u32,
-//         directory_max_depth: u32,
-//         bucket_max_size: usize,
-//     ) -> Self {
-//         Self {
-//             index_name: name,
-//             bpm,
-//             cmp,
-//             hash_fn,
-//             header_max_depth,
-//             directory_max_depth,
-//             bucket_max_size,
-//             header_page_id: INVALID_PAGE_ID,
-//         }
-//     }
-//
-//     pub fn insert(&self, key: K, value: V, transaction: &Transaction) -> bool {
-//         unimplemented!();
-//     }
-//
-//     pub fn remove(&self, key: &K, transaction: &Transaction) -> bool {
-//         unimplemented!();
-//     }
-//
-//     pub fn get_value(&self, key: &K, result: &mut Vec<V>, transaction: &Transaction) -> bool {
-//         unimplemented!();
-//     }
-//
-//     pub fn verify_integrity(&self) {
-//         unimplemented!();
-//     }
-//
-//     pub fn get_header_page_id(&self) -> PageId {
-//         self.header_page_id
-//     }
-//
-//     pub fn print_ht(&self) {
-//         unimplemented!();
-//     }
-//
-//     fn hash(&self, key: &K) -> u32 {
-//         let mut hasher = self.hash_fn.clone();
-//         hasher.write(&key);
-//         hasher.finish() as u32
-//     }
-//
-//     fn insert_to_new_directory(
-//         &self,
-//         header: &ExtendableHTableHeaderPage,
-//         directory_idx: u32,
-//         hash: u32,
-//         key: &K,
-//         value: &V,
-//     ) -> bool {
-//         unimplemented!();
-//     }
-//
-//     fn insert_to_new_bucket(
-//         &self,
-//         directory: &ExtendableHTableDirectoryPage,
-//         bucket_idx: u32,
-//         key: &K,
-//         value: &V,
-//     ) -> bool {
-//         unimplemented!();
-//     }
-//
-//     fn update_directory_mapping(
-//         &self,
-//         directory: &ExtendableHTableDirectoryPage,
-//         new_bucket_idx: u32,
-//         new_bucket_page_id: PageId,
-//         new_local_depth: u32,
-//         local_depth_mask: u32,
-//     ) {
-//         unimplemented!();
-//     }
-//
-//     fn migrate_entries(
-//         &self,
-//         old_bucket: &mut ExtendableHTableBucketPage<K, V, C>,
-//         new_bucket: &mut ExtendableHTableBucketPage<K, V, C>,
-//         new_bucket_idx: u32,
-//         local_depth_mask: u32,
-//     ) {
-//         unimplemented!();
-//     }
-// }
-//
+    /// Removes a key-value pair from the hash table.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to delete.
+    /// * `transaction` - The current transaction (optional).
+    ///
+    /// # Returns
+    ///
+    /// `true` if the remove succeeded, `false` otherwise.
+    pub fn remove(&self, key: &K, transaction: Option<&Transaction>) -> bool {
+        // TODO(P2): Add implementation
+        unimplemented!()
+    }
+
+    /// Gets the value associated with a given key in the hash table.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to look up.
+    /// * `result` - The value(s) associated with the given key.
+    /// * `transaction` - The current transaction (optional).
+    ///
+    /// # Returns
+    ///
+    /// `true` if the lookup succeeded, `false` otherwise.
+    pub fn get_value(&self, key: &K, result: &mut Vec<V>, transaction: Option<&Transaction>) -> bool {
+        // TODO(P2): Add implementation
+        unimplemented!()
+    }
+
+    /// Helper function to verify the integrity of the extendible hash table's directory.
+    pub fn verify_integrity(&self) {
+        // TODO: Add implementation
+        unimplemented!()
+    }
+
+    /// Helper function to expose the header page id.
+    pub fn get_header_page_id(&self) -> u32 {
+        self.header_page_id
+    }
+
+    /// Helper function to print out the HashTable.
+    pub fn print_ht(&self) {
+        // TODO: Add implementation
+        unimplemented!()
+    }
+
+    /// Hash - simple helper to downcast MurmurHash's 64-bit hash to 32-bit
+    /// for extendible hashing.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to hash.
+    ///
+    /// # Returns
+    ///
+    /// The down-casted 32-bit hash.
+    fn hash(&self, key: &K) -> u32 {
+        // TODO: Add implementation
+        unimplemented!()
+    }
+
+    fn insert_to_new_directory(
+        &self,
+        header: &ExtendableHTableHeaderPage,
+        directory_idx: u32,
+        hash: u32,
+        key: &K,
+        value: &V,
+    ) -> bool {
+        // TODO: Add implementation
+        unimplemented!()
+    }
+
+    fn insert_to_new_bucket(
+        &self,
+        directory: &ExtendableHTableDirectoryPage,
+        bucket_idx: u32,
+        key: &K,
+        value: &V,
+    ) -> bool {
+        // TODO: Add implementation
+        unimplemented!()
+    }
+
+    fn update_directory_mapping(
+        &self,
+        directory: &ExtendableHTableDirectoryPage,
+        new_bucket_idx: u32,
+        new_bucket_page_id: u32,
+        new_local_depth: u32,
+        local_depth_mask: u32,
+    ) {
+        // TODO: Add implementation
+        unimplemented!()
+    }
+
+    fn migrate_entries(
+        &self,
+        old_bucket: &ExtendableHTableBucketPage<K, V, C>,
+        new_bucket: &ExtendableHTableBucketPage<K, V, C>,
+        new_bucket_idx: u32,
+        local_depth_mask: u32,
+    ) {
+        // TODO: Add implementation
+        unimplemented!()
+    }
+}
--- src/container/hash_table.rs
+++ src/container/hash_table.rs
@@ -1,44 +1,6 @@
-use std::vec::Vec;
-use crate::concurrency::transaction::Transaction;
 
-
-pub trait DiskHashTable<KeyType, ValueType, KeyComparator> {
-    /// Inserts a key-value pair into the hash table.
-    ///
-    /// # Arguments
-    ///
-    /// * `transaction` - The current transaction.
-    /// * `key` - The key to insert.
-    /// * `value` - The value to be associated with the key.
-    ///
-    /// # Returns
-    ///
-    /// `true` if the insert succeeded, `false` otherwise.
-    fn insert(&self, transaction: &Transaction, key: &KeyType, value: &ValueType) -> bool;
-
-    /// Deletes the associated value for the given key.
-    ///
-    /// # Arguments
-    ///
-    /// * `transaction` - The current transaction.
-    /// * `key` - The key to delete.
-    /// * `value` - The value to delete.
-    ///
-    /// # Returns
-    ///
-    /// `true` if the remove succeeded, `false` otherwise.
-    fn remove(&self, transaction: &Transaction, key: &KeyType, value: &ValueType) -> bool;
-
-    /// Performs a point query on the hash table.
-    ///
-    /// # Arguments
-    ///
-    /// * `transaction` - The current transaction.
-    /// * `key` - The key to look up.
-    /// * `result` - The value(s) associated with the given key.
-    ///
-    /// # Returns
-    ///
-    /// `true` if the lookup succeeded, `false` otherwise.
-    fn get_value(&self, transaction: &Transaction, key: &KeyType, result: &mut Vec<ValueType>) -> bool;
+pub trait HashTable<K, V> {
+    fn find(key: K, value: V) -> bool;
+    fn remove(key:K) -> bool;
+    fn insert(key: K, value:V) -> ();
 }
