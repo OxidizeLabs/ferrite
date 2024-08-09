7847a0ec9bd4344915adfa7cea196524912480dd
TK <thomas.korrison@clausematch.com>
2024-08-09 09:49:42 +0100

    Update hash function
    - Refactor for to work with generic types

--- tests/container/hash_functions_tests.rs
+++ tests/container/hash_functions_tests.rs
@@ -5,10 +5,34 @@ mod tests {
     use super::HashFunction;
 
     #[test]
-    fn test_hash_function() {
-        let hash_function = HashFunction::<[u8]>::new();
-        let key = b"test_key";
-        let hash = hash_function.get_hash(key);
+    fn test_hash_function_with_i32() {
+        let hash_function = HashFunction::<i32>::new();
+        let key = 42;
+        let hash = hash_function.get_hash(&key);
+        assert_ne!(hash, 0);
+    }
+
+    #[test]
+    fn test_hash_function_with_u32() {
+        let hash_function = HashFunction::<u32>::new();
+        let key = 42u32;
+        let hash = hash_function.get_hash(&key);
+        assert_ne!(hash, 0);
+    }
+
+    #[test]
+    fn test_hash_function_with_str() {
+        let hash_function = HashFunction::<&str>::new();
+        let key = "test_key";
+        let hash = hash_function.get_hash(&key);
+        assert_ne!(hash, 0);
+    }
+
+    #[test]
+    fn test_hash_function_with_string() {
+        let hash_function = HashFunction::<String>::new();
+        let key = String::from("test_key");
+        let hash = hash_function.get_hash(&key);
         assert_ne!(hash, 0);
     }
 }
