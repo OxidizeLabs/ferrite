b1a1940d7655637fc29626020066560676d3b18a
TK <thomas.korrison@clausematch.com>
2024-08-16 12:15:14 +0100

    Update page guards
    - Fix deadlocks in page guard constructors and destructors

--- src/storage/page/page_guard.rs
+++ src/storage/page/page_guard.rs
@@ -7,7 +7,6 @@ use std::sync::Arc;
 use log::{debug, info};
 
 /// BasicPageGuard is a structure that helps manage access to a page in the buffer pool.
-#[derive(Clone)]
 pub struct BasicPageGuard {
     bpm: Option<Arc<BufferPoolManager>>,
     page: Option<Arc<RwLock<Page>>>,
@@ -24,28 +23,18 @@ impl BasicPageGuard {
     /// # Returns
     /// A new `BasicPageGuard` instance.
     pub fn new(bpm: Arc<BufferPoolManager>, page: Arc<RwLock<Page>>) -> Self {
-        let page_id = page.read().get_page_id();
+        let page_id = {
+            // Acquire a read lock on the page only long enough to get the page ID.
+            let page_guard = page.read();
+            page_guard.get_page_id()
+        };
+
         info!("Creating new BasicPageGuard for page ID {}", page_id);
+
         Self {
             bpm: Some(bpm),
             page: Some(page),
             is_dirty: false,
-        }
-    }
-
-    /// Drops a `BasicPageGuard`.
-    ///
-    /// Dropping a page guard should clear all contents
-    /// (so that the page guard is no longer useful), and
-    /// it should tell the BPM that we are done using this page,
-    /// per the specification in the writeup.
-    fn drop_guard(&mut self) {
-        if let (Some(bpm), Some(page)) = (self.bpm.take(), self.page.take()) {
-            let mut page_guard = page.write();
-            let page_id = page_guard.get_page_id();
-            page_guard.decrement_pin_count();
-            bpm.unpin_page(page_id, self.is_dirty, AccessType::Unknown);
-            info!("Dropped BasicPageGuard for page ID {}", page_id);
         }
     }
 
@@ -110,14 +99,14 @@ impl BasicPageGuard {
         page.get_page_id()
     }
 
-    /// Returns a reference to the data.
+    /// Returns an owned copy of the data.
     pub fn get_data(&self) -> Box<[u8; DB_PAGE_SIZE]> {
         let page = self.page.as_ref().unwrap().read();
         debug!("Fetching data for page ID {}", page.get_page_id());
         Box::new(*page.get_data())
     }
 
-    /// Returns a mutable reference to the data and marks the page as dirty.
+    /// Returns an owned mutable copy of the data and marks the page as dirty.
     pub fn get_data_mut(&mut self) -> Box<[u8; DB_PAGE_SIZE]> {
         self.is_dirty = true;
         let mut page = self.page.as_mut().unwrap().write();
@@ -147,14 +136,26 @@ impl BasicPageGuard {
 
 impl Drop for BasicPageGuard {
     fn drop(&mut self) {
-        if let Some(bpm) = self.bpm.take() {
-            if let Some(page) = self.page.take() {
-                let mut page_guard = page.write();
-                let page_id = page_guard.get_page_id();
-                page_guard.decrement_pin_count();
-                bpm.unpin_page(page_id, self.is_dirty, AccessType::Unknown);
-                info!("Dropped BasicPageGuard for page ID {}", page_id);
+        info!("Dropping BasicPageGuard for page ID");
+
+        // Extract references without holding locks
+        if let (Some(bpm), Some(page)) = (self.bpm.take(), self.page.take()) {
+            let page_id = {
+                let page_guard = page.read(); // Only lock the page briefly to get the ID
+                page_guard.get_page_id()
+            };
+
+            // Perform all BufferPoolManager-related operations separately
+            {
+                let mut page_write_guard = page.write();
+                page_write_guard.decrement_pin_count();
             }
+
+            {
+                bpm.unpin_page(page_id, self.is_dirty, AccessType::Unknown);
+            }
+
+            info!("Dropped BasicPageGuard for page ID {}", page_id);
         }
     }
 }
@@ -181,61 +182,40 @@ impl ReadPageGuard {
         }
     }
 
-    /// Move constructor for `ReadPageGuard`.
-    ///
-    /// Very similar to `BasicPageGuard`. You want to create
-    /// a `ReadPageGuard` using another `ReadPageGuard`. Think
-    /// about if there's any way you can make this easier for yourself...
-    pub fn from(other: ReadPageGuard) -> Self {
-        info!("Moving ReadPageGuard for page ID {}", other.page_id());
-        Self {
-            guard: BasicPageGuard::from(other.guard.clone()),
-        }
-    }
-
-    /// Move assignment for `ReadPageGuard`.
-    ///
-    /// Very similar to `BasicPageGuard`. Given another `ReadPageGuard`,
-    /// replace the contents of this one with that one.
-    pub fn assign(&mut self, other: ReadPageGuard) -> &Self {
-        info!("Assigning ReadPageGuard for page ID {}", other.page_id());
-        self.guard.assign(other.guard.clone());
-        self
-    }
-
-    /// Destructor for `ReadPageGuard`.
-    ///
-    /// Just like with `BasicPageGuard`, this should behave
-    /// as if you were dropping the guard.
-    pub fn drop(&mut self) {
-        self.guard.drop_guard();
-    }
-
     /// Returns the page ID.
     pub fn page_id(&self) -> PageId {
         self.guard.page_id()
     }
 
-    /// Returns a reference to the data.
+    /// Returns an owned copy of the data.
     pub fn get_data(&self) -> Box<[u8; DB_PAGE_SIZE]> {
         self.guard.get_data()
-    }
-
-
-    /// Returns a mutable reference to the data casted to a specific type and marks the page as dirty.
-    ///
-    /// # Returns
-    /// A mutable reference to the data as a specific type.
-    pub fn as_type_mut<T>(&mut self) -> &mut T {
-        self.guard.is_dirty = true;
-        let mut data = self.guard.get_data_mut();
-        unsafe { &mut *(data.as_mut_ptr() as *mut T) }
     }
 }
 
 impl Drop for ReadPageGuard {
     fn drop(&mut self) {
-        self.guard.drop_guard();
+        info!("Dropping ReadPageGuard for page ID");
+
+        // Extract references without holding locks
+        if let (Some(bpm), Some(page)) = (self.guard.bpm.take(), self.guard.page.take()) {
+            let page_id = {
+                let page_guard = page.read(); // Only lock the page briefly to get the ID
+                page_guard.get_page_id()
+            };
+
+            // Perform all BufferPoolManager-related operations separately
+            {
+                let mut page_write_guard = page.write();
+                page_write_guard.decrement_pin_count();
+            }
+
+            {
+                bpm.unpin_page(page_id, self.guard.is_dirty, AccessType::Unknown);
+            }
+
+            info!("Dropped ReadPageGuard for page ID {}", page_id);
+        }
     }
 }
 
@@ -261,64 +241,44 @@ impl WritePageGuard {
         }
     }
 
-    /// Move constructor for `WritePageGuard`.
-    ///
-    /// Very similar to `BasicPageGuard`. You want to create
-    /// a `WritePageGuard` using another `WritePageGuard`. Think
-    /// about if there's any way you can make this easier for yourself...
-    pub fn from(other: WritePageGuard) -> Self {
-        info!("Moving WritePageGuard for page ID {}", other.page_id());
-        Self {
-            guard: BasicPageGuard::from(other.guard.clone()),
-        }
-    }
-
-    /// Move assignment for `WritePageGuard`.
-    ///
-    /// Very similar to `BasicPageGuard`. Given another `WritePageGuard`,
-    /// replace the contents of this one with that one.
-    pub fn assign(&mut self, other: WritePageGuard) -> &Self {
-        info!("Assigning WritePageGuard for page ID {}", other.page_id());
-        self.guard.assign(other.guard.clone());
-        self
-    }
-
-    /// Destructor for `WritePageGuard`.
-    ///
-    /// Just like with `BasicPageGuard`, this should behave
-    /// as if you were dropping the guard.
-    pub fn drop(&mut self) {
-        self.guard.drop_guard();
-    }
-
     /// Returns the page ID.
     pub fn page_id(&self) -> PageId {
         self.guard.page_id()
     }
 
-    /// Returns a reference to the data.
+    /// Returns an owned copy of the data.
     pub fn get_data(&self) -> Box<[u8; DB_PAGE_SIZE]> {
         self.guard.get_data()
     }
 
-    /// Returns a mutable reference to the data.
+    /// Returns an owned mutable copy of the data.
     pub fn get_data_mut(&mut self) -> Box<[u8; DB_PAGE_SIZE]> {
         self.guard.get_data_mut()
-    }
-
-    /// Returns a mutable reference to the data casted to a specific type and marks the page as dirty.
-    ///
-    /// # Returns
-    /// A mutable reference to the data as a specific type.
-    pub fn as_type_mut<T>(&mut self) -> &mut T {
-        self.guard.is_dirty = true;
-        let mut data = self.get_data_mut();
-        unsafe { &mut *(data.as_mut_ptr() as *mut T) }
     }
 }
 
 impl Drop for WritePageGuard {
     fn drop(&mut self) {
-        self.guard.drop_guard();
+        info!("Dropping WritePageGuard for page ID");
+
+        // Extract references without holding locks
+        if let (Some(bpm), Some(page)) = (self.guard.bpm.take(), self.guard.page.take()) {
+            let page_id = {
+                let page_guard = page.read(); // Only lock the page briefly to get the ID
+                page_guard.get_page_id()
+            };
+
+            // Perform all BufferPoolManager-related operations separately
+            {
+                let mut page_write_guard = page.write();
+                page_write_guard.decrement_pin_count();
+            }
+
+            {
+                bpm.unpin_page(page_id, self.guard.is_dirty, AccessType::Unknown);
+            }
+
+            info!("Dropped WritePageGuard for page ID {}", page_id);
+        }
     }
 }
