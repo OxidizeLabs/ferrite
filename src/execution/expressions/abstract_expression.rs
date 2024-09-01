13bb48a6c37172ee061ef06551976580f90b3ce3
TK <thomas.korrison@clausematch.com>
2024-09-01 05:20:18 +0100

    Add abstract_expression.rs

--- src/execution/expressions/abstract_expression.rs
+++ src/execution/expressions/abstract_expression.rs
@@ -1,95 +1,122 @@
-use std::fmt;
-use std::sync::Arc;
-
 use crate::catalogue::column::Column;
 use crate::catalogue::schema::Schema;
+use crate::execution::expressions::arithmetic_expression::ArithmeticExpression;
+use crate::execution::expressions::column_value_expression::ColumnRefExpression;
+use crate::execution::expressions::comparison_expression::ComparisonExpression;
+use crate::execution::expressions::constant_value_expression::ConstantExpression;
 use crate::storage::table::tuple::Tuple;
+use crate::types_db::type_id::TypeId;
 use crate::types_db::value::Value;
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::rc::Rc;
 
-/// AbstractExpression is the base class of all the expressions in the system.
-/// Expressions are modeled as trees, i.e., every expression may have a variable number of children.
-pub trait AbstractExpression: fmt::Debug + Send + Sync {
-    /// Evaluates the expression with the given tuple and schema.
-    ///
-    /// # Parameters
-    /// - `tuple`: The tuple to evaluate.
-    /// - `schema`: The schema of the tuple.
-    ///
-    /// # Returns
-    /// The value obtained by evaluating the expression.
-    fn evaluate(&self, tuple: &Tuple, schema: &Schema) -> Value;
 
-    /// Evaluates the expression for a JOIN.
-    ///
-    /// # Parameters
-    /// - `left_tuple`: The left tuple.
-    /// - `left_schema`: The schema of the left tuple.
-    /// - `right_tuple`: The right tuple.
-    /// - `right_schema`: The schema of the right tuple.
-    ///
-    /// # Returns
-    /// The value obtained by evaluating a JOIN on the left and right tuples.
-    fn evaluate_join(
-        &self,
-        left_tuple: &Tuple,
-        left_schema: &Schema,
-        right_tuple: &Tuple,
-        right_schema: &Schema,
-    ) -> Value;
-
-    /// Returns the child at the specified index.
-    ///
-    /// # Parameters
-    /// - `child_idx`: The index of the child to return.
-    ///
-    /// # Returns
-    /// A reference to the child at the specified index.
-    fn get_child_at(&self, child_idx: usize) -> Option<Arc<dyn AbstractExpression>>;
-
-    /// Returns the children of this expression. The order may matter.
-    ///
-    /// # Returns
-    /// A vector of references to the children of this expression.
-    fn get_children(&self) -> Vec<Arc<dyn AbstractExpression>>;
-
-    /// Returns the return type of this expression.
-    ///
-    /// # Returns
-    /// The return type of this expression.
-    fn get_return_type(&self) -> Column;
-
-    /// Returns a string representation of the expression and its children.
-    ///
-    /// # Returns
-    /// A string representation of the expression.
-    fn to_string(&self) -> String;
-
-    /// Clones the expression with new children.
-    ///
-    /// # Parameters
-    /// - `children`: The new children of the expression.
-    ///
-    /// # Returns
-    /// A new expression with the specified children.
-    fn clone_with_children(
-        &self,
-        children: Vec<Arc<dyn AbstractExpression>>,
-    ) -> Arc<dyn AbstractExpression>;
+#[derive(Debug, Clone)]
+pub enum Expression {
+    Constant(ConstantExpression),
+    ColumnRef(ColumnRefExpression),
+    Arithmetic(ArithmeticExpression),
+    Comparison(ComparisonExpression),
 }
 
-/// A macro to define the clone_with_children method for implementing structs.
-#[macro_export]
-macro_rules! impl_clone_exp_with_children {
-    ($struct_name:ident) => {
-        fn clone_with_children(
-            &self,
-            children: Vec<Arc<dyn AbstractExpression>>,
-        ) -> Arc<dyn AbstractExpression> {
-            let mut expr = $struct_name {
-                children,
-                ..self.clone()
-            };
-            Arc::new(expr)
+impl Expression {
+    pub fn evaluate(&self, tuple: &Tuple, schema: &Schema) -> Result<Value, String> {
+        match self {
+            Expression::Constant(expr) => Ok(expr.get_value().clone()),
+            Expression::ColumnRef(expr) => Ok(tuple.get_value(expr.get_column_index()).clone()),
+            Expression::Arithmetic(expr) => expr.evaluate(tuple, schema),
+            Expression::Comparison(expr) => expr.evaluate(tuple, schema),
         }
-    };
+    }
+
+    pub fn evaluate_join(&self, left_tuple: &Tuple, left_schema: &Schema, right_tuple: &Tuple, right_schema: &Schema) -> Result<Value, String> {
+        match self {
+            Expression::Constant(expr) => Ok(expr.get_value().clone()),
+            Expression::ColumnRef(expr) => {
+                // Determine which tuple to use based on the column index
+                if expr.get_column_index() < left_schema.get_column_count() as usize {
+                    let l_value = left_tuple.get_value(expr.get_column_index());
+                    Ok(l_value.clone())
+                } else {
+                    let r_value = right_tuple.get_value(expr.get_column_index() - left_schema.get_column_count() as usize);
+                    Ok(r_value.clone())
+                }
+            }
+            Expression::Arithmetic(expr) => expr.evaluate_join(left_tuple, left_schema, right_tuple, right_schema),
+            Expression::Comparison(expr) => expr.evaluate_join(left_tuple, left_schema, right_tuple, right_schema),
+            _ => Err("Evaluate Join Error".to_string())
+        }
+    }
+
+    pub fn get_children(&self) -> Vec<&Expression> {
+        match self {
+            Expression::Constant(_) | Expression::ColumnRef(_) => vec![],
+            Expression::Arithmetic(expr) => vec![&expr.get_left(), &expr.get_right()],
+            Expression::Comparison(expr) => vec![&expr.get_left(), &expr.get_right()],
+        }
+    }
+
+    pub fn get_return_type(&self) -> &Column {
+        match self {
+            Expression::Constant(expr) => &expr.get_ret_type(),
+            Expression::ColumnRef(expr) => &expr.get_ret_type(),
+            Expression::Arithmetic(expr) => &expr.get_ret_type(),
+            Expression::Comparison(expr) => &expr.get_ret_type(),
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        match self {
+            Expression::Constant(expr) => format!("Constant({:?})", expr.get_value()),
+            Expression::ColumnRef(expr) => format!("ColumnRef({})", expr.get_column_index()),
+            Expression::Arithmetic(expr) => format!("({:?} {:?} {:?})", expr.get_left(), expr.get_op(), expr.get_right()),
+            Expression::Comparison(expr) => format!("({:?} {:?} {:?})", expr.get_left(), expr.get_comp_type(), expr.get_right()),
+        }
+    }
+
+    pub fn clone_with_children(&self, children: Vec<Rc<Expression>>) -> Rc<Expression> {
+        match self {
+            Expression::Constant(expr) => Rc::new(Expression::Constant(expr.clone())),
+            Expression::ColumnRef(expr) => Rc::new(Expression::ColumnRef(expr.clone())),
+            Expression::Arithmetic(expr) => Rc::new(Expression::Arithmetic(ArithmeticExpression::new(
+                children[0].clone(),
+                children[1].clone(),
+                expr.get_op()).unwrap()
+            )),
+            Expression::Comparison(expr) => Rc::new(Expression::Comparison(ComparisonExpression::new(
+                children[0].clone(),
+                children[1].clone(),
+                expr.get_comp_type(),
+            )))
+        }
+    }
+}
+
+// Implement Display for Expression
+impl Display for Expression {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use super::*;
+
+    #[test]
+    fn constant_expression() {
+        let value = Value::new(42);
+        let ret_type = Column::new("const", TypeId::Integer);
+        let expr = Expression::Constant(ConstantExpression::new(value.clone(), ret_type));
+
+        let schema = Schema::new(vec![]);
+        let tuple = Tuple::new(vec![], schema.clone(), 0);
+
+        assert_eq!(expr.evaluate(&tuple, &schema).unwrap(), value);
+        assert_eq!(expr.get_children().len(), 0);
+        assert_eq!(expr.to_string(), "Constant(Integer(42))");
+    }
+
+    // Add more tests for other expression types and operations
 }
