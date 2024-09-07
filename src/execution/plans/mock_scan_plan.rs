65069a170eb0e7d236c324ae852beda84d8b6509
TK <thomas.korrison@clausematch.com>
2024-09-07 15:06:24 +0100

    Make mock_scan_plan.rs work

--- src/execution/plans/mock_scan_plan.rs
+++ src/execution/plans/mock_scan_plan.rs
@@ -1,44 +1,69 @@
 use crate::catalogue::schema::Schema;
 use crate::execution::plans::abstract_plan::{AbstractPlanNode, PlanNode, PlanType};
-use std::rc::Rc;
+use std::sync::Arc;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct MockScanNode {
-    output_schema: Rc<Schema>,
-    child: Option<Rc<PlanNode>>,
+    output_schema: Arc<Schema>,
+    children: Vec<PlanNode>,
 }
 
 impl MockScanNode {
-    pub fn new(output_schema: Rc<Schema>, child: Option<Rc<PlanNode>>) -> Self {
+    pub fn new(output_schema: Arc<Schema>, children: Vec<PlanNode>) -> Self {
         Self {
             output_schema,
-            child,
+            children,
         }
     }
 }
 
 impl AbstractPlanNode for MockScanNode {
+    /// Returns a reference to the output schema of this node.
     fn get_output_schema(&self) -> &Schema {
         &self.output_schema
     }
 
+    /// Returns a reference to the child nodes of this node.
+    ///
+    /// Note: Currently, DeleteNode only has one child, but this method
+    /// returns an empty vector for consistency with the trait definition.
     fn get_children(&self) -> &Vec<PlanNode> {
-        todo!()
+        &self.children
     }
 
+    /// Returns the type of this plan node.
     fn get_type(&self) -> PlanType {
-        todo!()
+        PlanType::MockScan
     }
 
+    /// Returns a string representation of this node.
+    ///
+    /// # Arguments
+    ///
+    /// * `with_schema` - If true, includes the schema in the string representation.
     fn to_string(&self, with_schema: bool) -> String {
-        todo!()
+        let mut result = String::from("MockScanNode");
+        if with_schema {
+            result.push_str(&format!(" [{}]", self.output_schema));
+        }
+        result
     }
 
+    /// Returns a string representation of this node, including the schema.
     fn plan_node_to_string(&self) -> String {
-        todo!()
+        self.to_string(true)
     }
 
+    /// Returns a string representation of this node's children.
+    ///
+    /// # Arguments
+    ///
+    /// * `indent` - The number of spaces to indent the output.
     fn children_to_string(&self, indent: usize) -> String {
-        todo!()
+        self.children
+            .iter()
+            .enumerate()
+            .map(|(i, child)| format!("\n{:indent$}Child {}: {}", "", i + 1, AbstractPlanNode::to_string(child, false), indent = indent))
+            .collect()
     }
 }
