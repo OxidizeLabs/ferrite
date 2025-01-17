77ca992f67db2128db91bc2c42fe8838ef2954d2
thomaskorrison <thomas_korrison@hotmail.com>
2025-01-17 12:13:55 +0000

    Refactor execution_context.rs

---
+++ src/execution/execution_context.rs
@@ -0,0 +1,102 @@
+use crate::buffer::buffer_pool_manager::BufferPoolManager;
+use crate::catalog::catalog::Catalog;
+use crate::execution::check_option::{CheckOption, CheckOptions};
+use crate::execution::executors::abstract_executor::AbstractExecutor;
+use crate::execution::transaction_context::TransactionContext;
+use log::debug;
+use parking_lot::RwLock;
+use std::collections::VecDeque;
+use std::sync::Arc;
+use crate::concurrency::lock_manager::LockManager;
+
+pub struct ExecutionContext {
+    buffer_pool_manager: Arc<BufferPoolManager>,
+    catalog: Arc<RwLock<Catalog>>,
+    transaction_context: Arc<TransactionContext>,
+    nlj_check_exec_set: VecDeque<(Box<dyn AbstractExecutor>, Box<dyn AbstractExecutor>)>,
+    check_options: Arc<CheckOptions>,
+    is_delete: bool,
+}
+
+impl ExecutionContext {
+    pub fn new(
+        buffer_pool_manager: Arc<BufferPoolManager>,
+        catalog: Arc<RwLock<Catalog>>,
+        transaction_context: Arc<TransactionContext>,
+    ) -> Self {
+        debug!(
+            "Creating ExecutorContext for transaction {}",
+            transaction_context.get_transaction_id()
+        );
+
+        // Create check options and enable necessary optimizations
+        let mut options = CheckOptions::new();
+        options.add_check(CheckOption::EnablePushdownCheck);
+        options.add_check(CheckOption::EnableTopnCheck);
+
+        Self {
+            buffer_pool_manager,
+            catalog,
+            transaction_context,
+            nlj_check_exec_set: VecDeque::new(),
+            check_options: Arc::new(options),
+            is_delete: false,
+        }
+    }
+
+    pub fn get_transaction_context(&self) -> Arc<TransactionContext> {
+        self.transaction_context.clone()
+    }
+
+    pub fn get_catalog(&self) -> &Arc<RwLock<Catalog>> {
+        &self.catalog
+    }
+
+    pub fn get_buffer_pool_manager(&self) -> &BufferPoolManager {
+        &self.buffer_pool_manager
+    }
+
+    pub fn get_nlj_check_exec_set(
+        &self,
+    ) -> &VecDeque<(Box<dyn AbstractExecutor>, Box<dyn AbstractExecutor>)> {
+        &self.nlj_check_exec_set
+    }
+
+    pub fn get_check_options(&self) -> Arc<CheckOptions> {
+        Arc::clone(&self.check_options)
+    }
+
+    pub fn set_check_options(&mut self, options: CheckOptions) {
+        debug!("Setting check options");
+        self.check_options = Arc::new(options);
+    }
+
+    pub fn add_check_option(
+        &mut self,
+        left_exec: Box<dyn AbstractExecutor>,
+        right_exec: Box<dyn AbstractExecutor>,
+    ) {
+        self.nlj_check_exec_set.push_back((left_exec, right_exec));
+    }
+
+    pub fn init_check_options(&mut self) {
+        let mut options = CheckOptions::new();
+
+        if !self.nlj_check_exec_set.is_empty() {
+            options.add_check(CheckOption::EnableNljCheck);
+        }
+
+        options.add_check(CheckOption::EnablePushdownCheck);
+        options.add_check(CheckOption::EnableTopnCheck);
+
+        self.check_options = Arc::new(options);
+    }
+
+    pub fn is_delete(&self) -> bool {
+        self.is_delete
+    }
+
+    pub fn set_delete(&mut self, is_delete: bool) {
+        self.is_delete = is_delete;
+    }
+}
