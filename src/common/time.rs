3254c9027234072e126078927369b5383b64c9eb
thomaskorrison <thomas_korrison@hotmail.com>
2025-01-21 19:50:35 +0000

    Add Time struct for Tuplemeta

--- src/common/time.rs
+++ src/common/time.rs
@@ -1,4 +1,6 @@
+use std::fmt::Display;
 use std::time::{SystemTime, UNIX_EPOCH};
+use serde::{Deserialize, Serialize};
 
 pub trait TimeSource: Send + Sync {
     fn now(&self) -> u64;
@@ -12,5 +14,27 @@ impl TimeSource for SystemTimeSource {
             .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_micros() as u64
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct TimeStamp(u64);
+
+impl TimeStamp {
+    pub const MAX: TimeStamp = TimeStamp(u64::MAX);
+    pub const MIN: TimeStamp = TimeStamp(0);
+
+    pub fn new(ts: u64) -> Self {
+        Self(ts)
+    }
+
+    pub fn get(&self) -> u64 {
+        self.0
+    }
+}
+
+impl Display for TimeStamp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
