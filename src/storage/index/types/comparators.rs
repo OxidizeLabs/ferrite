c11fbdfc71936d883799e238bfabd1c0cc33efbb
Thomas Korrison <tkorrison@daltontx.com>
2025-05-05 19:09:00 +0100

    Add key and value type traits with implementations for i32 and String
    Introduced new traits for key and value types in the index module, allowing for flexible key management. Implemented the KeyType trait for i32 and String, providing methods for conversion to and from storage values. Added a comparator for i32 keys to facilitate ordering. This enhances the index structure's capability to handle various data types effectively.

---
+++ src/storage/index/types/comparators.rs
@@ -0,0 +1,23 @@
+use crate::storage::index::types::key_types::KeyType;
+use std::cmp::Ordering;
+
+/// Type for i32 comparators
+pub type I32Comparator = fn(&i32, &i32) -> Ordering;
+
+/// Standard comparator for i32 keys
+pub fn i32_comparator(a: &i32, b: &i32) -> Ordering {
+    a.cmp(b)
+}
+
+/// Trait for key comparators
+pub trait KeyComparator<K: KeyType>: Fn(&K, &K) -> Ordering {
+    // Additional functionality can be added here
+}
+
+// Implement KeyComparator for function types
+impl<K> KeyComparator<K> for fn(&K, &K) -> Ordering
+where
+    K: KeyType,
+{
+    // Implementation of any additional methods would go here
+} 
---
+++ src/storage/index/types/key_types.rs
@@ -0,0 +1,47 @@
+use crate::types_db::type_id::TypeId;
+use crate::types_db::types::Type;
+use crate::types_db::value::{Val, Value};
+
+/// Trait for types that can be used as keys in indexes
+pub trait KeyType: Sized + Clone + PartialEq + PartialOrd {
+    /// Convert this key to a Value for storage
+    fn to_value(&self) -> Value;
+
+    /// Create a key from a Value
+    fn from_value(value: &Value) -> Result<Self, String>;
+
+    /// Get the TypeId for this key type
+    fn type_id() -> TypeId;
+}
+
+impl KeyType for i32 {
+    fn to_value(&self) -> Value {
+        Value::new_with_type(Val::Integer(*self), TypeId::Integer)
+    }
+
+    fn from_value(value: &Value) -> Result<Self, String> {
+        value.as_integer()
+    }
+
+    fn type_id() -> TypeId {
+        TypeId::Integer
+    }
+}
+
+impl KeyType for String {
+    fn to_value(&self) -> Value {
+        Value::new_with_type(Val::VarLen(self.clone()), TypeId::VarChar)
+    }
+
+    fn from_value(value: &Value) -> Result<Self, String> {
+        match value.get_val() {
+            Val::VarLen(s) => Ok(s.clone()),
+            Val::ConstLen(s) => Ok(s.clone()),
+            _ => Err(format!("Cannot convert {:?} to String", value)),
+        }
+    }
+
+    fn type_id() -> TypeId {
+        TypeId::VarChar
+    }
+} 
