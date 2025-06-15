d05496adc6c77e9223cd8790890da89e43ff4df3
Thomas Korrison <thomas_korrison@hotmail.com>
2025-06-15 12:25:42 +0100

    Remove unused imports and helper methods in `log_iterator` tests
    - Deleted unused imports from `log_iterator` test module.
    - Removed unused helper methods `write_record`, `create_test_begin_record`, and `create_test_commit_record`, improving code clarity.

--- src/recovery/log_iterator.rs
+++ src/recovery/log_iterator.rs
@@ -189,8 +189,6 @@ impl LogIterator {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::common::config::{Lsn, TxnId, INVALID_LSN};
-    use crate::recovery::log_record::{LogRecord, LogRecordType};
     use std::fs;
     use std::path::Path;
     use std::sync::Arc;
@@ -238,25 +236,6 @@ mod tests {
             if Path::new(&self.log_path).exists() {
                 fs::remove_file(&self.log_path).unwrap();
             }
-        }
-
-        async fn write_record(&self, record: &LogRecord) {
-            self.disk_manager.write_log(record).await.unwrap();
-        }
-
-        fn create_test_begin_record(&self, txn_id: TxnId) -> LogRecord {
-            let record =
-                LogRecord::new_transaction_record(txn_id, INVALID_LSN, LogRecordType::Begin);
-            // Set a dummy LSN
-            record.set_lsn(txn_id as Lsn);
-            record
-        }
-
-        fn create_test_commit_record(&self, txn_id: TxnId, prev_lsn: Lsn) -> LogRecord {
-            let record = LogRecord::new_transaction_record(txn_id, prev_lsn, LogRecordType::Commit);
-            // Set a dummy LSN
-            record.set_lsn((txn_id + 1) as Lsn);
-            record
         }
     }
 
