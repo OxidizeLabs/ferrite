83b0c34bb659d7b66d9bfa3fa0dfe1f1d6f4ed73
Thomas Korrison <thomas_korrison@hotmail.com>
2025-06-26 17:49:18 +0100

    Remove unused `CheckpointManager` struct and references
    - Commented out the unused `CheckpointManager` struct and associated imports to reduce warnings.
    - Simplified the file by cleaning up redundant code.

--- src/recovery/checkpoint_manager.rs
+++ src/recovery/checkpoint_manager.rs
@@ -1,20 +1,10 @@
-use crate::buffer::buffer_pool_manager_async::BufferPoolManager;
-use crate::concurrency::transaction_manager::TransactionManager;
-use crate::recovery::log_manager::LogManager;
-use std::sync::Arc;
-
-pub struct CheckpointManager {
-    log_manager: Arc<LogManager>,
-    transaction_manager: Arc<TransactionManager>,
-    buffer_pool_manager: Arc<BufferPoolManager>,
-}
-
-impl CheckpointManager {
-    pub fn new(
-        p0: Arc<TransactionManager>,
-        p1: Arc<LogManager>,
-        p2: Arc<BufferPoolManager>,
-    ) -> Self {
-        todo!()
-    }
-}
+// use crate::buffer::buffer_pool_manager_async::BufferPoolManager;
+// use crate::concurrency::transaction_manager::TransactionManager;
+// use crate::recovery::log_manager::LogManager;
+// use std::sync::Arc;
+//
+// pub struct CheckpointManager {
+//     log_manager: Arc<LogManager>,
+//     transaction_manager: Arc<TransactionManager>,
+//     buffer_pool_manager: Arc<BufferPoolManager>,
+// }
