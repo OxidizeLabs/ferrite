3e102bab68f07eab511ff8a8d52e4719fde549cd
Thomas Korrison <thomas_korrison@hotmail.com>
2025-08-31 13:24:08 +0100

    rename create  index integration test module

--- tests/execution/mod.rs
+++ tests/execution/mod.rs
@@ -1,7 +1,7 @@
 pub mod engine_smoke;
 pub mod execution_context;
 pub mod engine_create_table;
-pub mod engine_index;
+pub mod engine_create_index;
 pub mod engine_dml;
 pub mod engine_select;
 pub mod engine_agg;
